c61713ccf78951d16b1f394151db9427c6ce8e42
Zachary <zacharyle25@gmail.com>
2021-05-11 16:49:20 -0400

    COMPLETED Bubble Sort. Successfully sorts up to a defined size

--- Bubble Sort/main.cpp
+++ Bubble Sort/main.cpp
@@ -1,5 +1,4 @@
 #include <random>
-#include <vector>
 #include <iostream>
 #include <math.h>
 
@@ -7,73 +6,85 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 
-std::vector<int> bubbleSort(std::vector<int> set, std::vector<sf::RectangleShape>* rect);
+void bubbleSort(std::vector<int>& set, std::vector<sf::RectangleShape>& rect);
 void printNumbers(std::vector<int> set);
 template<typename T>
 void swap(T& a, T& b);
-void updateRectangle(std::vector<sf::RectangleShape>* rect, int index);
+std::string getPair(sf::Vector2f position);
 
 #define SIZE 50
+#define WINDOWSIZE 800
+
+struct rectangles {
+    sf::Vector2f position;
+    int height;
+} data[SIZE];
 
 int main(int argc, char* argv[]) {
     std::random_device rd;
     std::default_random_engine gen;
-    std::uniform_int_distribution<int> dist(1, 100);
-    sf::Vector2f windowSize(750, 750);
-    std::vector<int> numbers;
-    std::vector<sf::RectangleShape> rect;
+    std::uniform_int_distribution<int> dist(1, WINDOWSIZE);
+    sf::RectangleShape rect;
+    int iter = 0;
+    int limit = SIZE - 1;
 
     for(int i = 0; i < SIZE; i++) {
-        numbers.push_back(dist(gen));
-        rect.push_back(sf::RectangleShape(sf::Vector2f(
-            windowSize.x / SIZE,
-            numbers[i] * 7)));
-        rect[i].setOrigin(rect[i].getPoint(3));
-        rect[i].setPosition(rect[i].getSize().x * i, windowSize.y);
-        rect[i].setFillColor(sf::Color::Red);
+        data[i].height = dist(gen);
+        data[i].position.x = WINDOWSIZE / SIZE;
+        data[i].position.y = WINDOWSIZE;
     }
 
-    sf::RenderWindow window(sf::VideoMode(windowSize.x, windowSize.y), "Bubble Sort");
+    sf::RenderWindow window(sf::VideoMode(WINDOWSIZE, WINDOWSIZE), "Bubble Sort");
     window.setFramerateLimit(60);
-
-    numbers = bubbleSort(numbers, &rect);
-    printNumbers(numbers);
 
     while(window.isOpen()) {
         sf::Event event;
-        while(window.pollEvent(event)) {
+        while(window.pollEvent(event))
             if(event.type == sf::Event::Closed)
                 window.close();
+
+        window.clear(sf::Color::Black);
+
+        for(int i = 0; i < SIZE; i++) {
+            rect.setRotation(180);
+            rect.setOrigin(rect.getPoint(1));
+            rect.setSize(sf::Vector2f(data[i].position.x, data[i].height));
+            rect.setPosition(data[i].position.x * i, WINDOWSIZE);
+            rect.setFillColor(sf::Color::White);
+            rect.setOutlineColor(sf::Color::Black);
+            if(data[iter].height > data[iter + 1].height)
+                swap(data[iter].height, data[iter + 1].height);
+            if(iter == i)
+                rect.setFillColor(sf::Color::Green);
+            rect.setOutlineThickness(0.75);
+            window.draw(rect);
         }
-
-        for(int i = 0; i < rect.size(); i++) {
-            window.draw(rect[i]);
-        }
-        /* for(int i = 0; i < numbers.size() - 1; i++) {
-            for(int j = 0; j < numbers.size() - i - 1; j++) {
-                if(numbers[j > numbers[j + 1]]) {
-
-                }
-            }
-        } */
-
         window.display();
+
+        iter++;
+        if(iter >= limit) {
+            iter = 0;
+            limit--;
+        }
     }
 
     return 0;
 }
 
-std::vector<int> bubbleSort(std::vector<int> set, std::vector<sf::RectangleShape>* rect) {
+void bubbleSort(std::vector<int>& set, std::vector<sf::RectangleShape>& rect) {
     if(set.empty())
         throw std::runtime_error("Invalid size in bubbleSort");
     for(int i = 0; i < set.size() - 1; i++) {
         for(int j = 0; j < set.size() - i - 1; j++) {
-            if(set[j] > set[j + 1])
+            if(set[j] > set[j + 1]) {
                 swap(set[j], set[j + 1]);
+                swap(rect[j], rect[j + 1]);
+                auto temp = rect[j].getPosition();
+                rect[j].setPosition(rect[j + 1].getPosition());
+                rect[j + 1].setPosition(temp);
+            }
         }
     }
-
-    return set;
 }
 
 void printNumbers(std::vector<int> set) {
@@ -90,4 +101,9 @@ void swap(T& a, T& b) {
     auto temp = a;
     a = b;
     b = temp;
+}
+
+std::string getPair(sf::Vector2f position) {
+    return "(" + std::to_string(position.x) +
+        ", " + std::to_string(position.y) + ")";
 }
